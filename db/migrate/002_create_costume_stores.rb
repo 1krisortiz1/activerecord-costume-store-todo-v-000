73dd99dd1bdbfe56dbbb645438432a0292644db9
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-01-02 19:26:08 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,10 @@
 # Create your costume_stores migration here
+def change
+    create_table :costume_stores do |c|
+        c.string :name
+        c.string :location
+        c.integer :costume_inventory
+        c.integer :num_of_employees
+        c.timestamps
+    end
+end
