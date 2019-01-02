8b6be6bb1e4adf42e2fbb1dc6021929dbef083ad
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-01-02 19:39:30 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,10 +5,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
             c.string :name
             c.string :location
             c.string :theme
-            c.integer :num_of_employees
-            c.boolean :still_in_business
+            c.integer :price
             c.datetime :opening_time
             c.datetime :closing_time
+            c.string :description
         end
     end
 end
