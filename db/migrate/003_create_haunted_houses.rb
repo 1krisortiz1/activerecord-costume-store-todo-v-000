fc30ba7b49078c2e931967953aa854b21c695602
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-01-02 19:38:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,10 +1,10 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     def change
-        create_table :costume_stores do |c|
+        create_table :haunted_houses do |c|
             c.string :name
             c.string :location
-            c.integer :costume_inventory
+            c.string :theme
             c.integer :num_of_employees
             c.boolean :still_in_business
             c.datetime :opening_time
